aa6c05e8e35289bf20be2519ba7c9264a34bdc6b
nicodemosranksss <nicolau.subtil.neto@escola.pr.gov.br>
2026-03-13 07:54:36 -0300

    create: Aula//130326

---
+++ aulas /130326.js
@@ -0,0 +1,11 @@
+let variavel = 30;
+let nome = 10
+
+function soma(a,b){
+    return a + b 
+}
+soma(lucro,lucro2);
+
+function resultadoDivisaoSocio(lucro, lucro2){
+      return soma(lucro, lucro2)\2
+}
