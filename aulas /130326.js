05f29e2b5f9058dfebc2ac0cdac3f14c655ab9b2
nicodemosranksss <nicolau.subtil.neto@escola.pr.gov.br>
2026-03-16 12:11:38 -0300

    git

--- aulas /130326.js
+++ aulas /130326.js
@@ -7,5 +7,20 @@ function soma(a,b){
 soma(lucro,lucro2);
 
 function resultadoDivisaoSocio(lucro, lucro2){
-      return soma(lucro, lucro2)\2
+      return soma(lucro, lucro2)/2
 }
+
+
+// Agora crie uma função que recebe um argumento do
+// tipo aritmético(+,-,/,*) e faça a operação 
+// com lucro e lucro2
+
+function calculadora(ari,a,b){
+    switch(ari){
+        case '+':
+        resultado = a + b ;
+        break; 
+       case '-' :
+    }
+    }
+
